dc09f0c715246a297643ae72ba4ee52ea1bcce19
mariyumsohail <mariyumsohail71@gmail.com>
2024-05-21 23:02:08 +0500

    index.html

---
+++ app.js
@@ -0,0 +1,173 @@
+// function welcomeCity() {
+//     // Taking city name as input from user
+//     var city = prompt("Enter city name:");
+
+//     // Check if the entered city is Karachi
+//     if (city.toLowerCase() === "karachi") {
+//         alert("Welcome to city of lights");
+//     } else {
+//         alert("Welcome to " + city);
+//     }
+// }
+
+// // welcomeCity();
+
+// question2
+//  
+//     // Taking gender as input from user
+//     var gender = prompt("Enter your gender (male/female):");
+//     gender = gender.toLowerCase();
+//     if (gender === "male") {
+//         alert("Good Morning Sir.");
+//     } else if (gender === "female") {
+//         alert("Good Morning Ma'am.");
+//     } else {
+//         alert("Invalid input. Please enter 'male' or 'female'.");
+//     }
+//   }
+
+
+
+// qustion4
+
+
+// var remainingFuel = +prompt("Enter the remaining fuel in your car (in liters):");
+// if (remainingFuel < 0.25) {
+//     alert("Please refill the fuel in your car.");
+// } else {
+//     alert("You have enough fuel in your car. Safe travels!");
+// }
+
+// question 5
+
+
+// // a. var a = 4;
+// if (++a === 5){
+//     alert("given condition for variable a is true");
+// }
+
+// // b. var b = 82;
+// if (b++ === 83){
+//     alert("given condition for variable b is true");
+// }
+
+// // c. var c = 12;
+// if (c++ === 13){
+//     alert("condition 1 is true");
+// }
+// if (c === 13){
+//     alert("condition 2 is true");
+// }
+// if (++c < 14){
+//     alert("condition 3 is true");
+// }
+// if(c === 14){
+//     alert("condition 4 is true");
+// }
+
+
+// if (totalCost === laborCost + materialCost){
+//     alert("The cost equals");
+// }
+
+// // e. if (true){
+//     alert("True");
+
+// if (false){
+//     alert("False");
+// }
+
+// // f. if("car" < "cat"){
+//     alert("car is smaller than cat");
+
+// question7
+
+
+      
+       
+          
+        //     var subject1Marks = +prompt("Enter marks obtained in subject 1:");
+        //     var subject2Marks = +prompt("Enter marks obtained in subject 2:");
+        //     var subject3Marks = +prompt("Enter marks obtained in subject 3:");
+        //     var totalMarks = +prompt("Enter total marks:");
+
+       
+        //     var totalObtainedMarks = subject1Marks + subject2Marks + subject3Marks;
+
+        //     var percentage = (totalObtainedMarks / totalMarks) * 100;
+
+        //     var grade;
+        //     if (percentage >= 90) {
+        //         grade = 'A+';
+        //     } else if (percentage >= 80) {
+        //         grade = 'A';
+        //     } else if (percentage >= 70) {
+        //         grade = 'B';
+        //     } else if (percentage >= 60) {
+        //         grade = 'C';
+        //     } else if (percentage >= 50) {
+        //         grade = 'D';
+        //     } else {
+        //         grade = 'Fail';
+        //     }
+
+         
+        //     alert("Total Marks: " + totalMarks +
+        //           "\nMarks Obtained: " + totalObtainedMarks +
+        //           "\nPercentage: " + percentage.toFixed(2) + "%" +
+        //           "\nGrade: " + grade);
+        // }
+      
+      
+
+//    qusetion8
+
+ 
+        //   var currentTime = Date.now();
+        //   var secretNumber = ((currentTime * 9301 + 49297) % 233280) / 233280 * 10 + 1 | 0;
+        //   var guess = +prompt("Guess the secret number (between 1 and 10):");
+        //   if (guess === secretNumber) {
+        //       alert("Bingo! Correct answer");
+        //   } else if (guess === secretNumber + 1 || guess === secretNumber - 1) {
+        //       alert("Close enough to the correct answer");
+        //   } else {
+        //       alert("Sorry, incorrect guess. The correct answer was " + secretNumber);
+        //   }
+
+
+        // question9
+
+
+            // var number = +prompt("Enter a number:");
+            // if (number % 3 === 0) {
+            //     alert(number + " is divisible by 3.");
+            // } else {
+            //     alert(number + " is not divisible by 3.");
+            // }
+       
+        // qusetion 10
+
+
+            // var number = +prompt("Enter a number:");
+            // if (number % 2 === 0) {
+            //     alert(number + " is an even number.");
+            // } else {
+            //     alert(number + " is an odd number.");
+            // }
+        
+
+             // qusetion 11
+
+            // var temperature = +prompt("Enter the temperature in Celsius:");
+            // if (temperature > 40) {
+            //     alert("It is too hot outside.");
+            // } else if (temperature > 30) {
+            //     alert("The Weather today is Normal.");
+            // } else if (temperature > 20) {
+            //     alert("Today’s Weather is cool.");
+            // } else if (temperature > 10) {
+            //     alert("OMG! Today’s weather is so Cool.");
+            // } else {
+            //     alert("It is freezing outside!");
+            // }
+        
